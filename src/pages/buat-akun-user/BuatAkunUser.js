b33e4ad309e75d7fa25f771db7f064e5c7346454
Nops <NAUVAL.21@mhs.unsyiah.ac.id>
2023-05-02 17:52:33 +0700

    Tampilan Buat Akun User
    - Membuat tampilan untuk pembuatan akun user.
    - Link kembali ke halaman login user.
    - Memperbaiki file buat akun user

--- src/pages/buat-akun-user/BuatAkunUser.js
+++ src/pages/buat-akun-user/BuatAkunUser.js
@@ -119,8 +119,8 @@ export default function BuatAkunUser() {
 
         <button className="tombol-buat-akun-user">Buat Akun</button>
 
-        <p className="sudanh-punya-akun-user-link">
-          Sudah punya akun?<Link to="/">Masuk</Link>
+        <p className="sudah-punya-akun-user-link">
+          Sudah punya akun?<Link to="/login-user" className="link-masuk-user">Masuk</Link>
         </p>
       </form>
     </div>
