662b5e0760925484e2608603bb84063fd68c414f
Adith <aditya98@mhs.unsyiah.ac.id>
2023-05-03 20:45:18 +0700

    Memperbaiki halaman login petugas

--- src/pages/login-petugas/LoginPetugas.js
+++ src/pages/login-petugas/LoginPetugas.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import { UserAuth } from "../../context/AuthContext";
+import "./LoginPetugas.css";
 
 export default function LoginPetugas() {
   const [formData, setFormData] = useState({
@@ -44,7 +45,7 @@ export default function LoginPetugas() {
           Silahkan masuk menggunakan akun Petugas Waste to Fresh Anda
         </p>
       </div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="form-login-petugas">
         <p>Alamat Email</p>
         <input
           className="input-form-login-petugas"
@@ -66,14 +67,17 @@ export default function LoginPetugas() {
         />
 
         <button className="tombol-login-petugas">Masuk</button>
+
+        <p className="buat-akun-link">
+          Belum punya akun?
+          <Link to="/buat-akun-petugas" className="link-buat-akun-petugas">
+            Buat Akun Petugas
+          </Link>
+        </p>
+        <Link to="/" className="login-petugas-link">
+          Masuk sebagai user
+        </Link>
       </form>
-      <p className="buat-akun-link">
-        Belum punya akun?
-        <Link to="/buat-akun-petugas">Buat Akun Petugas</Link>
-      </p>
-      <Link to="/" className="login-petugas-link">
-        Masuk sebagai user
-      </Link>
     </div>
   );
 }
