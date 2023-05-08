e1732b4aa61f9c2c06be65def70fb011ebd17a5c
M Keysha Al Yassar <keysha@mhs.unsyiah.ac.id>
2023-05-09 00:08:21 +0700

    Halaman Rincian Notifikasi Tukar Poin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext";
 
 // Import Halaman
